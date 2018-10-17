a45dc7cc8d089002abf15405c7e3607b677068eb
aj03794 <adamjohnston151@yahoo.com>
2018-10-17 09:07:37 -0400

    Refactoring to listen for gcp events

---
+++ src/workflow/index.js
@@ -0,0 +1,6 @@
+// Subscribe to gcp events
+// Reach out to github and download the release into downloads folder
+// Unzip the download
+// Delete the zip
+// Turn off old version of the app running
+// Turn on the new version of the app
